31353440fee7a33851aea30b9695d7cc62225ccf
puleugo <puleugo@gmail.com>
2023-04-13 15:52:19 +0900

    feat: TypeORM(PostgreSQL) 구성

--- admin-2711/src/app/app.module.ts
+++ admin-2711/src/app/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { AuthModule } from './auth/auth.module';
 import { MemberModule } from './member/member.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceConfig } from './data-source';
 
 @Module({
-  imports: [HealthCheckModule, AuthModule, MemberModule],
+  imports: [
+    HealthCheckModule,
+    AuthModule,
+    MemberModule,
+    TypeOrmModule.forRoot(dataSourceConfig),
+  ],
 })
 export class AppModule {}
