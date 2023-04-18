f133928bcc16fc093833c11fa25b57d156978cf7
puleugo <puleugo@gmail.com>
2023-04-18 16:11:31 +0900

    feat: dataSource initialize

--- admin-2711/src/main.ts
+++ admin-2711/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
+import { dataSource } from './app/data-source';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+
+  await dataSource.initialize();
+
   await app.listen(3000);
 }
 
